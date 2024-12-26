ff5e382bdcc0b543d1791666509bbeb336c3ede1
sahil <sahilvernekar96@gmail.com>
2024-12-26 22:06:31 +0530

    placeOrder function

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 const menu = [
-    {name: "Margherita", price: 8},
-    {name: "Pepperoni", price: 10},
-    {name: "Dominator", price: 10},
-    {name: "Veggie", price: 9},
+    { name: "Margherita", price: 8 },
+    { name: "Pepperoni", price: 10 },
+    { name: "Dominator", price: 10 },
+    { name: "Veggie", price: 9 },
 ];
 
 const cashInRegister = 100;
 const orderQueue = [];
 
-function addNewPizza(pizzaObj){
+function addNewPizza(pizzaObj) {
     menu.push(pizzaObj);
 }
 
-function placeOrder(pizzaName){
-    const pizzaItem = menu.find(item => item.name === pizzaName);
+function placeOrder(pizzaName) {
+    const selectedPizza = menu.find(item => item.name === pizzaName);
 
-    if (!pizzaItem) {
+    if (!selectedPizza) {
         console.log("Pizza is not on the menu.");
     }
-    
+
+    cashInRegister += selectedPizza.price;
+    const newOrder = {pizza: selectedPizza, status: "ordered"}
+    orderQueue.push(newOrder);
+
+    return newOrder;
 }
