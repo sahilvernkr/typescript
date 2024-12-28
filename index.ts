7d6a8b79be0f115df01fab7800b21b4b56e05dc3
sahil <sahilvernekar96@gmail.com>
2024-12-28 13:31:19 +0530

    nested type objects

--- index.ts
+++ index.ts
@@ -2,20 +2,37 @@ let myName: string = "Bob"
 let numberOfWheels: number = 4
 let isStudent: boolean = false
 
+type Address = {
+    streets: string
+    city: string
+    country: string
+}
+
 type Person = {
     name: string
     age: number
     isStudent: boolean
+    address: Address
 }
 
 let person1: Person = {
     name: "Joe",
     age: 23,
-    isStudent: true
+    isStudent: true,
+    address:{
+        streets: "123 str",
+        city: "margao",
+        country: "India"
+    }
 }
 
 let person2: Person = {
     name: "Doe",
     age: 45,
-    isStudent: false
+    isStudent: false,
+    address:{
+        streets: "123 BV",
+        city: "Hilversum",
+        country: "Netherland"
+    }
 }
